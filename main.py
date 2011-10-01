e3978c89d83283721633592b88fe863b2947cf4c
Wagner Sartori Junior <wsartori@gmail.com>
2011-10-01 20:45:09 -0300

    Adding GPLv3 licensing

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 # -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2011 Wagner Sartori Junior <wsartori@gmail.com>
+http://www.wsartori.com
+
+This file is part of James.
+
+James program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 from twisted.python import log, usage
 from twisted.internet import reactor, task
 from twisted.internet.serialport import SerialPort
